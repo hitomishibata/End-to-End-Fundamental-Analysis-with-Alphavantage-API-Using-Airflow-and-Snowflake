d70981f07b5567612d2e86a22e452b32947d2b74
Hitomi Shibata <hitomi@LAPTOP-9NUGRGEB>
2024-06-11 14:19:29 +0200

    modified the airflow dag file

--- airflow_env/dags/ingest_raw_json_file.py
+++ airflow_env/dags/ingest_raw_json_file.py
@@ -8,15 +8,13 @@ from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
-from airflow.contrib.hooks.snowflake_operator import SnowflakeOperator
-from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv()
 api_key = os.getenv("API_KEY")
-#Set the cloud object storage path
-base = ObjectStoragePath("s3://container name/", conn_id="idname")
+BUCKET = 'stockmarket-airflow-dev-bucket'
 base_url = "https://finnhub.io/api/v1"
 
 default_args = {
@@ -38,24 +36,22 @@ default_args = {
 def ingest_raw_stock_market_json_file():
 
     @task()
-    def ingest_raw_json_file(url, file_name):
+    def ingest_raw_json_file(url, file_name, bucket):
         response = requests.get(url)
         response.raise_for_status()
         json_data = response.json()
         json_object = json.dumps(json_data, indent=4)
-        path = base/f"raw_{file_name}.json"
-        with path.open("w") as outfile:
+        with open(f"raw_{file_name}.json", "w") as outfile:
             outfile.write(json_object)
-        return path
         
     @task()
     def ready():
         pass
 
-    ingest_raw_json_file(f"{base_url}/stock/metric?symbol=AAPL&metric=all&token={api_key}", "basic_financials")
-    ingest_raw_json_file(f"{base_url}/stock/profile2&token={api_key}", "company_profile2")
-    ingest_raw_json_file(f"{base_url}/stock/financials-reported&token={api_key}", "financials_reported")
-    ingest_raw_json_file(f"{base_url}/stock/earnings?symbol=AAPL&token={api_key}", "stock_earnings_url")
+    ingest_raw_json_file(f"{base_url}/stock/metric?symbol=AAPL&metric=all&token={api_key}", "basic_financials", "stockmarket-airflow-dev-bucket")
+    ingest_raw_json_file(f"{base_url}/stock/profile2&token={api_key}", "company_profile2", "stockmarket-airflow-dev-bucket")
+    ingest_raw_json_file(f"{base_url}/stock/financials-reported&token={api_key}", "financials_reported", "stockmarket-airflow-dev-bucket")
+    ingest_raw_json_file(f"{base_url}/stock/earnings?symbol=AAPL&token={api_key}", "stock_earnings_url", "stockmarket-airflow-dev-bucket")
     ready()
 
 ingest_raw_stock_market_json_file()
