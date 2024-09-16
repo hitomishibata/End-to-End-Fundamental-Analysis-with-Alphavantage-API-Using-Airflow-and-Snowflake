b0c27e01da3ab28cfaa2d687d8075afa1afdac5c
Hitomi Shibata <hitomiei0225@outlook.jp>
2024-09-16 03:08:07 +0200

    modified the data_ingestion and webscraping file

--- ingest_data/data_ingestion.py
+++ ingest_data/data_ingestion.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import csv
+import re
 import boto3
 from botocore.exceptions import ClientError
 import logging
@@ -18,34 +20,17 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-SYMBOLS = ["AAPL",]
+SYMBOLS = ['AAPL', 'AXP', 'BA', 'CAT', 'CSCO', 'CVX', 'DIS', 'DOW', 'GS', 'HD']
 TOPICS = ["OVERVIEW", "ETF_PROFILE", "DIVIDENDS", "INCOME_STATEMENT", "BALANCE_SHEET", "CASH_FLOW", "EARNINGS"]
 #make a tuple from the two lists (symbols and topics)
 #a symbol has to be coupled with all elements in topics.[(topic1, symbol1), (topic2, symbol1)...]
 BUCKET_NAME = "alphavantage-stock-market-ap-northeast-3-dev-s3"
 
-def create_pair_of_topic_symbol(symbols: list, topics: list):
-    for symbol in SYMBOLS:
-        for topic in TOPICS:
-            pair = (topic, symbol)
-    return pair
-
-def get_csv_from_api(topic: str):
-    csv_url = f"https://www.alphavantage.co/query?function={topic}&apikey={env.get('API_KEY')}"
-    c = pd.read_csv(csv_url)
-
-def get_json_from_api(topic: str, symbol: str|None) -> None:    
-    if symbol is None:
-        url = f"https://www.alphavantage.co/query?function={topic}&apikey={env.get('API_KEY')}"
-    else:
-        url = f"https://www.alphavantage.co/query?function={topic}&symbol={symbol}&apikey={env.get('API_KEY')}"
+def ingest_json_from_api_to_s3(topic, symbol) -> None:   
+    url = f"https://www.alphavantage.co/query?function={topic}&symbol={symbol}&apikey={env.get('API_KEY')}"
     response = requests.get(url)
-    #should I convert the json data into python object?
     dict_object = response.json()
-    json_data = json.dumps(dict_object)
-    return json_data
-
-def upload_file(file_name, bucket, object_name):
+    data = bytes(json.dumps(dict_object).encode('UTF-8'))
     s3_client = boto3.client(
         service_name='s3',
         region_name=env.get("region"),
@@ -53,18 +38,16 @@ def upload_file(file_name, bucket, object_name):
         aws_secret_access_key=env.get("aws_secret_access_key")
     )
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
+        response = s3_client.put_object(Body=data, Bucket=BUCKET_NAME, Key=f"raw/{topic}_{symbol}.json")
     except ClientError as e:
         logging.error(e)
         return False
     return True
 #how can i pass the object name?
-csv_file = get_csv_from_api("LISTING_STATUS")
-csv_file2 = get_csv_from_api("EARNINGS_CALENDAR")
-upload_file(csv_file, BUCKET_NAME, "raw/listing_status.csv")
-upload_file(csv_file2, BUCKET_NAME, "raw/earnings_calendar.csv")
-#get_json_from_api(create_pair_of_topic_symbol(SYMBOLS, TOPICS),BUCKET_NAME, f"raw/{}.json")
 
+for symbol in SYMBOLS:
+    for topic in TOPICS:
+        ingest_json_from_api_to_s3(topic, symbol)
 
 
 
--- ingest_data/webscraping_top_companies.py
+++ ingest_data/webscraping_top_companies.py
@@ -1,16 +1,17 @@
-import yfinance as yf
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import requests
 
 #get the top stock active companies from yahoo finance
 #put them in a list
-
-URL = "https://finance.yahoo.com/markets/stocks/most-active/?guccounter=1&start=0&count=100"
-page = urlopen(URL)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-print(soup.prettify())
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url = "https://finance.yahoo.com/markets/stocks/most-active/"
+response = requests.get(url, headers=headers)
+print(response)
+soup = BeautifulSoup(response.content, "lxml")
+for item in soup.select('.markets-table'):
+    print(item.select('[span.class="symbol.yf-ravs5v"]')[0].get_text())
+#print(soup.prettify())
 #get the useful element using find_all 
 #s = soup.find("", class="")
 #content = s.find_all("tag")
